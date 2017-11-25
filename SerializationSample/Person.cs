e3790c2c77511f22a2d7fadd7106e3a9d3a5d333
bubibubi <u.ballestrazzi@gmail.com>
2017-11-25 11:10:46 +0100

    First commit

---
+++ SerializationSample/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationSample
+{
+public class Person
+{
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
+    public int Age { get; set; }
+    public Person Mom { get; set; }
+    public Person Dad { get; set; }
+}
+}
---
+++ SerializationSample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace SerializationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person me = new Person();
+            me.FirstName = "Umberto";
+            me.SecondName = "Ballestrazzi";
+
+            Person mom = new Person();
+            mom.FirstName = "myMom";
+            mom.SecondName = "Ballestrazzi";
+            mom.Dad = new Person { FirstName = "mygranpa", SecondName = "Ballestrazzi" };
+
+            Person dad = new Person();
+            dad.FirstName = "myDad";
+            dad.SecondName = "Ballestrazzi";
+
+            me.Dad = dad;
+            me.Mom = mom;
+
+            // Serializzazione - Viene utilizzato uno stream particolare (StringWriter)
+            // che scrive una stringa in memoria che poi andiamo a stampare
+            StringWriter stream = new StringWriter();
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            serializer.Serialize(stream, me);
+            Console.Write(stream);
+            Console.ReadLine();
+
+        }
+    }
+}
